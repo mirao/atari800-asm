3469ec88a445872f20433b264b2f0ca6610d2d56
Jaromir Obr <jaromir.obr@gmail.com>
2023-03-26 20:37:14 +0200

    gtia_gr0: Added a rotation of colors. It can be paused by a key press.

--- gtia_gr0/gtia_gr0.asm
+++ gtia_gr0/gtia_gr0.asm
@@ -6,10 +6,14 @@
 
 CHBAS_CUSTOM_PAGE = $3400
 
+RTCLOK2 = $14
 SAVMSC = $58
+LAST_CHAR_SET_ID = 15; Index of last character in character set
+LAST_CHAR_SCREEN_ID = $cc ; Index of character saved on the last position on screen. It determines a char color, e.g. 15 (orange) or 7 (blue)
 GPRIOR = $26f
 CHBAS = $2f4
 COLOR4 = $2c8
+CH = $2fc
 
     org $600
 
@@ -17,18 +21,51 @@ COLOR4 = $2c8
     mva #[%1100 0000] GPRIOR ; Enable GTIA GRAPHICS 11 to get 16 colors
     mva #6 COLOR4 ; Set a luminence
     
-    ; Display first 16 characters in GR0
-    ldy #15
-display_color
-    tya
+    ; Initialize last character (color), it will be decreased with every rotation to get a rotation effect
+    ldy #LAST_CHAR_SET_ID + 1
+    sty LAST_CHAR_SCREEN_ID
+
+    ; Wait for 1/60 sec to refresh screen
+wait_screen_refresh
+    lda RTCLOK2
+wait_screen_refresh_inner
+    cmp RTCLOK2
+    beq wait_screen_refresh_inner
+    
+    ; Wait for key press
+    lda #$ff
+    cmp CH
+    beq continue_animation ; No key pressed
+    ; A key pressed
+    sta CH
+    ; Stop animation and wait for one more key to continue
+wait_for_another_key
+    cmp CH
+    beq wait_for_another_key
+    sta CH
+
+continue_animation
+    ; Display first 16 characters in GR0 with position shifted to the right from last state
+    ldy #LAST_CHAR_SET_ID
+    ldx LAST_CHAR_SCREEN_ID
+get_next_char
+    dex
+    bpl is_the_char_last
+    ldx #LAST_CHAR_SET_ID
+is_the_char_last
+    cpy #LAST_CHAR_SET_ID
+    bne save_char
+    ; Store last character index for next rotation
+    stx LAST_CHAR_SCREEN_ID
+save_char
+    txa
     sta (SAVMSC), y-
-    bpl display_color
-wait
-    jmp wait
+    bpl get_next_char
+    jmp wait_screen_refresh
 
 ; Set 16 characters in a new character set, every char by different color
 .macro set_chbas
-    ldy #16 * 8 - 1 ; 16 characters, each for one color
+    ldy #(LAST_CHAR_SET_ID + 1) * 8 - 1 ; 16 characters, each for one color
     lda #$ff ; Start from last character (#15) that will have orange color in both nibbles
 set_chbas_char
     ldx #8 ; Each character has 8 lines
